d71f950810666747f96d0f345fccd016035d5c43
olena.qa45 <olena.qa45@gmail.com>
2026-03-31 00:25:33 +0300

    MNFCT-2 added dotenv folder library for configurations, env folder, refactor code of first test added dataclass library for simplification of configs, fixtures added library faker for random data generation

--- tests/second_test.py
+++ tests/second_test.py
@@ -1,9 +1,10 @@
 from playwright.sync_api import Page, expect
 
-#check that Start for free button is functional on the header
+
+# check that Start for free button is functional on the header
 def test_start_for_free(page: Page):
     page.goto("https://testomat.io")
     expect(page).to_have_title('AI Test Management Tool | Testomat.io')
     page.locator("#header").get_by_role("link", name="Start for free").click()
     expect(page).to_have_url("https://app.testomat.io/users/sign_up")
-    #expect(page).to_have_title('Testomat.io')
+    # expect(page).to_have_title('Testomat.io')
--- tests/third_test.py
+++ tests/third_test.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import Page, expect
 
-#check that Start for free button2 is functional on #welcome_section. Open in New page
+
+# check that Start for free button2 is functional on #welcome_section. Open in New page
 def test_start_for_free_2(page: Page):
     page.goto("https://testomat.io")
     expect(page).to_have_title('AI Test Management Tool | Testomat.io')
@@ -11,7 +12,8 @@ def test_start_for_free_2(page: Page):
     new_page.wait_for_load_state()
     expect(new_page).to_have_url("https://app.testomat.io/users/sign_up")
 
-#check that Start for free button2 is functional on #welcome_section. Open in the same tab
+
+# check that Start for free button2 is functional on #welcome_section. Open in the same tab
 def test_start_for_free_3(page: Page):
     page.goto("https://testomat.io")
     expect(page).to_have_title('AI Test Management Tool | Testomat.io')
@@ -20,3 +22,49 @@ def test_start_for_free_3(page: Page):
     )
     page.locator("#welcome_section").get_by_role("link", name="Start for free").click()
     expect(page).to_have_url("https://app.testomat.io/users/sign_up")
+
+    # some text
+
+
+def test_search_project_in_company(page: Page):
+    # arrange
+    page.goto(LOGIN_URL)
+    login_user(page, EMAIL, PASSWORD)
+
+    # act
+    target_project = "Brown PLC"
+    search_for_project(page, target_project)
+
+    # assert
+    expect(page.get_by_title(target_project)).to_be_visible()
+    expect(page.get_by_role("heading", name=target_project)).to_be_visible()
+    expect(page.locator("ul li h3", has_text=target_project)).to_be_visible()
+    expect(page.locator("ul li h3").filter(has_text=target_project)).to_have_text(target_project)
+
+    # create new project some broken test
+    # def test_create_new_project(page: Page):
+    # page.goto("https://app.testomat.io/users/sign_in")
+    # login_user(page, "olena.qa45@gmail.com", "fkjhadfgnj")
+
+    # page.locator("#company_id").click()
+    # page.locator("#company_id").select_option("Free Projects")
+
+    # page.get_by_role("button", name="Create project").click()
+    # page.get_by_role("link", name="href=/projects/new").click()
+
+    # assigned functions
+
+
+def search_for_project(page: Page, target_project: str):
+    expect(page.get_by_role("searchbox", name="Search")).to_be_visible()
+    page.locator("#content-desktop #search").fill(target_project)
+
+
+def open_home_page(page: Page):
+    page.goto(os.getenv("BASE_URL"))
+
+
+def login_user(page: Page, email: str, password: str):
+    page.locator("#content-desktop #user_email").fill(email)
+    page.locator("#content-desktop #user_password").fill(password)
+    page.get_by_role("button", name="Sign in").click()
