dd694ea08182a5c1a15d13cd4bc81d39e286a3e0
olena.qa45 <olena.qa45@gmail.com>
2026-04-05 19:47:57 +0300

    MNFCT-2 (#2)
    Add:
    - .env folder library for configurations, refactor code of first test
    - dataclass library for simplification of configs, fixtures
    - library faker for random data generation

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,62 +1,63 @@
+import os
+
+import pytest
+from faker import Faker
 from playwright.sync_api import (Page, expect)
 
-def test_login_with_invalid_creds(page: Page):
+
+@pytest.fixture(scope="function")
+def login(page: Page, configs):
+    page.goto(configs.login_url)
+    login_user(page, configs.email, configs.password)
+
+TARGET_PROJECT: str = "Brown PLC"
+
+# check login with invalid creds is not possible
+def test_login_with_invalid_creds(page: Page, configs):
     # arrange
     open_home_page(page)
     expect(page.locator("[href*='sign_in'].login-item")).to_be_visible()
 
     # act
-    page.get_by_text("Log in",exact=True).click()
-    login_user(page, "olena.qa45@gmail.com", "awwqedfcdfdf1!s")
+    page.get_by_text("Log in", exact=True).click()
+
+    invalid_password = Faker().password(length=10)
+
+    login_user(page, configs.email, invalid_password)
 
     # assert
-    expect(page.locator("#content-desktop").get_by_text("Invalid Email or password.")).to_be_visible()
-    # expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid Email or password.")
+    expect(page.locator("#content-desktop").get_by_text("Invalid email or password.")).to_be_visible()
+    expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid email or password.")
 
 
 # search target_project by title
-def test_search_project_in_company(page: Page):
-    # arrange
-    page.goto("https://app.testomat.io/users/sign_in")
-    login_user(page, "olena.qa45@gmail.com", "olololl")
+def test_search_project_in_company(page: Page, login):
 
-    # act
-    target_project = "Brown PLC"
-    search_for_project(page, target_project)
+    search_for_project(page, TARGET_PROJECT)
 
-    # assert
-    expect(page.get_by_title(target_project)).to_be_visible()
-    expect(page.get_by_role("heading", name=target_project)).to_be_visible()
-    # expect(page.locator("ul li h3",has_text=target_project)).to_be_visible()
-    # expect(page.locator("ul li h3").filter(has_text=target_project)).to_have_text(target_project)
+    expect(page.get_by_title(TARGET_PROJECT)).to_be_visible()
+    expect(page.get_by_role("heading", name=TARGET_PROJECT)).to_be_visible()
 
 
 # open free projects
-def test_should_be_possible_to_open_free_project(page: Page):
-    # arrange
-    page.goto("https://app.testomat.io/users/sign_in")
-    login_user(page, "olena.qa45@gmail.com", "fkjhadfgnj")
-    # act
+def test_should_be_possible_to_open_free_project(page: Page, login):
+
     page.locator("#company_id").click()
     page.locator("#company_id").select_option("Free Projects")
     # assert
-    target_project: str = "Brown PLC"
-    search_for_project(page, target_project)
+    search_for_project(page, TARGET_PROJECT)
 
-    expect(page.get_by_role("heading", name=target_project)).to_be_hidden()
+    expect(page.get_by_role("heading", name=TARGET_PROJECT)).to_be_hidden()
     expect(page.get_by_text("You have not created any projects yet")).to_be_visible(timeout=10000)
 
 
-# create new project some broken test
-# def test_create_new_project(page: Page):
-# page.goto("https://app.testomat.io/users/sign_in")
-# login_user(page, "olena.qa45@gmail.com", "fkjhadfgnj")
+# create new project button is visible
+def test_create_new_project(page: Page, login):
 
-# page.locator("#company_id").click()
-# page.locator("#company_id").select_option("Free Projects")
+    page.locator("#company_id").click()
+    page.locator("#company_id").select_option("Free Projects")
 
-# page.get_by_role("button", name="Create project").click()
-# page.get_by_role("link", name="href=/projects/new").click()
+    expect(page.get_by_role("link", name="Create project")).to_be_visible()
 
 
 # assigned functions
@@ -66,7 +67,7 @@ def search_for_project(page: Page, target_project: str):
 
 
 def open_home_page(page: Page):
-    page.goto("https://testomat.io")
+    page.goto(os.getenv("BASE_URL"))
 
 
 def login_user(page: Page, email: str, password: str):
